93d4998b24090f6950aa9d49f398793482888194
Vignesh <dnc@SLC-LPT-010>
2025-04-21 23:58:48 +0530

    Commit - EmployeeInformation

---
+++ EmployeeInformation/src/org/emp/Employee.java
@@ -0,0 +1,29 @@
+package org.emp;
+
+import org.client.Client;
+import org.company.Company;
+import org.project.Project;
+
+public class Employee {
+	
+	public void empName() {
+		System.out.println("Employee Name: Vishwa");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		employee.empName();
+		
+		Company company = new Company();
+		company.companyName();
+		
+		Client client = new Client();
+		client.clientName();
+		
+		Project project = new Project();
+		project.projectName();
+		
+	}
+
+}
