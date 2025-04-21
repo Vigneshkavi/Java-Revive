beae71f79a5bc8a2953a09cdfbe16ea90b6b82d2
Vignesh <dnc@SLC-LPT-010>
2025-04-21 23:48:00 +0530

    Commit - CollegeInformation

---
+++ CollegeInformation/src/org/college/College.java
@@ -0,0 +1,35 @@
+package org.college;
+
+public class College {
+	
+	private void collegeName() {
+		System.out.println("College Name: Paavai Institutions");
+	}
+	
+	private void collegeCode() {
+		System.out.println("College Code: 2003");
+	}
+	
+	private void collegeRank() {
+		System.out.println("College Rank: 1st");
+	}
+
+	public static void main(String[] args) {
+		College college = new College();
+		college.collegeName();
+		college.collegeCode();
+		college.collegeRank();
+		
+		Student student = new Student();
+		student.studentName();
+		student.studentDept();
+		student.studentId();
+		
+		Hostel hostel = new Hostel();
+		hostel.hostelName();
+		
+		Dept dept = new Dept();
+		dept.deptName();
+	}
+
+}
---
+++ CollegeInformation/src/org/college/Student.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class Student {
+	
+	public void studentName() {
+		System.out.println("Student Name: Kathir");
+	}
+	
+	public void studentDept() {
+		System.out.println("Student Department: ECE");
+	}
+	
+	public void studentId() {
+		System.out.println("Student ID: 66");
+	}
+
+}
