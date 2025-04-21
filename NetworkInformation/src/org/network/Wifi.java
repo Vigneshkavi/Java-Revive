5dd2139639aa4cebcd9ce0d262e27765814ccd47
Vignesh <dnc@SLC-LPT-010>
2025-04-21 23:59:51 +0530

    Commit - NetworkInformation

---
+++ NetworkInformation/src/org/network/Wifi.java
@@ -0,0 +1,23 @@
+package org.network;
+
+public class Wifi {
+	
+	private void wifiName() {
+		System.out.println("SSID Name: *****");
+	}
+	
+	public static void main(String[] args) {
+		Wifi wifi = new Wifi();
+		wifi.wifiName();
+		
+		MobileData md = new MobileData();
+		md.dataName();
+		
+		Lan lan = new Lan();
+		lan.lanName();
+		
+		Wireless wl = new Wireless();
+		wl.modemName();
+	}
+
+}
